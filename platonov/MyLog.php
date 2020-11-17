f62667e0d83ccafe355009afbae0edc7664ce65b
GreatDevide <65541956+GreatDevide@users.noreply.github.com>
2020-11-17 19:11:25 +0300

    lab9

--- platonov/MyLog.php
+++ platonov/MyLog.php
@@ -2,7 +2,7 @@
 
 use core\LogAbstract;
 use core\LogInterface;
-
+use DateTime;
 
 Class MyLog extends LogAbstract implements LogInterface {
 
@@ -19,9 +19,24 @@ Class MyLog extends LogAbstract implements LogInterface {
     
     public function _write(){
         
+        $d = new DateTime();
+        $filename = "log/".$d->format('d.m.Y_T_H.i.s.u').".log";
+
+        $dirname = "log";
+
+        if(!(is_dir($dirname)))
+        {
+            mkdir($dirname, 0777, true);
+        }
+
+        $logfile = "";
+        
         foreach($this->log as $value){
             echo $value."\n";
+            $logfile .= $value."\r\n";
         }
+        
+        file_put_contents($filename, $logfile, FILE_APPEND);
         
     }
     
